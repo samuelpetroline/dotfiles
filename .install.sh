843c3c47d4ca99e8fce2ee2922ebf1588650f104
Samuel Petroline <samuelpetroline@gmail.com>
2025-03-23 19:55:08 +0000

    chore: remove configuration file copying from installation script; add fast-syntax-highlighting, zsh-autosuggestions, and powerlevel10k as submodules

--- .install.sh
+++ .install.sh
@@ -43,10 +43,6 @@ rm -rf /tmp/SFMono_Nerd_Font/
 
 curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v1.0.4/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
 
-# Copy Configuration Files
-echo "Copying configuration files..."
-zsh .copy.sh
-
 # Start Services
 echo "Starting Services (grant permissions)..."
 skhd --start-service
